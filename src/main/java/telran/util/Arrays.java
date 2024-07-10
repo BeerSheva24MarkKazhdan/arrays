e233c97383aebfea6d16be4030332c9c2b252535
Mark <kazhdan.mark@gmail.com>
2024-07-10 23:12:27 +0300

    Added insert and remove methods and tests

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -1,6 +1,7 @@
 package telran.util;
 
 public class Arrays {
+
 public static int search(int [] array, int key){
 int index = 0;
 while (index < array.length && key !=array [index]) {
@@ -8,10 +9,25 @@ while (index < array.length && key !=array [index]) {
 }
 return index == array.length ? -1 : index;
 }
+
 public static int[] addNumber(int [] array, int numbers) {
 int[] res = java.util.Arrays.copyOf(array, array.length + 1);
 res[array.length] = numbers;
     return res;
 }
 
+public static int[] insert(int[] array, int index, int number) {
+    int [] res = java.util.Arrays.copyOf(array,array.length+1);
+    System.arraycopy(array,0,res,0,index);
+    System.arraycopy(array,index,res,index+1,array.length-index);
+    res[index] = number;
+ return res;   
+}
+
+public static int[] remove(int[]array, int index) {
+    int [] res = java.util.Arrays.copyOf(array,array.length-1);
+    System.arraycopy(array,0,res,0,index);
+    System.arraycopy(array, index+1, res,index, res.length-index);
+    return res;
+}
 }
--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -4,20 +4,61 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static telran.util.Arrays.*;
+
 public class ArraysTest {
 int[] numbers = {10, 7, 12, -4, 13, 3, 14};
+
 @Test
 void searchTest() {
     assertEquals (0, search(numbers, 10));
     assertEquals (6, search(numbers, 14));
     assertEquals (3, search(numbers, -4));
     assertEquals (-1, search(numbers, 100));
+    assertFalse(numbers.length==0);
 }
+
 @Test
 void addTest() {
     int newNumber = 100;
-    int [] expected = {10, 7, 12, -4, 13, 3, 100};
+    int [] expected = {10, 7, 12, -4, 13, 3, 14, newNumber};
     assertArrayEquals(expected, addNumber(numbers, newNumber));
+    assertFalse(numbers.length==0);
+}
 
+@Test
+void insertTest() {
+    int number=100;
+    int index = 0;
+    int [] array = {5,6,9,1,5,3,8};
+    int [] expected = {number,5,6,9,1,5,3,8};
+    assertArrayEquals(expected, insert(array, index , number));
+    index = 7;
+    int [] expected2 = {5,6,9,1,5,3,8,number};
+    assertArrayEquals(expected2, insert(array, index , number));
+    index = 4;
+    int [] expected3 = {5,6,9,1,number,5,3,8};
+    assertArrayEquals(expected3, insert(array, index , number));
+    assertFalse(array.length==0);
+}
+
+@Test
+void removeTest() {
+int index=0;
+int [] array = {5,6,9,1,5};
+int [] expected = {6,9,1,5};
+assertArrayEquals(expected, remove(array, index));
+index = 1;
+int [] expected2 = {5,9,1,5};
+assertArrayEquals(expected2, remove(array, index));
+index = 2;
+int [] expected3 = {5,6,1,5};
+assertArrayEquals(expected3, remove(array, index));
+index = 3;
+int [] expected4 = {5,6,9,5};
+assertArrayEquals(expected4, remove(array, index));
+index = 4;
+int [] expected5 = {5,6,9,1};
+assertArrayEquals(expected5, remove(array, index));
+assertFalse(array.length==0);
 }
 }
