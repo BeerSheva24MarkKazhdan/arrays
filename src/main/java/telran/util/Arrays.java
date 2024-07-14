62d4acdf8fe67bd1badb5e4ffdab85785a0cfce7
Mark <kazhdan.mark@gmail.com>
2024-07-14 22:45:23 +0300

    Added 3 new methods and tests

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -22,10 +22,84 @@ public static int[] insert(int[] array, int index, int number) {
     res[index] = number;
  return res;   
 }
-
+ 
 public static int[] remove(int[]array, int index) {
     int [] res = java.util.Arrays.copyOf(array,array.length-1);
-    System.arraycopy(array,index+1,res,index,array.length-index-1);
+    System.arraycopy(array,index+1,res,index,res.length-index);
     return res;
+}
+
+//HW 6
+
+public static boolean pushMaxAtEnd(int[]array, int length){
+    boolean res=true;
+
+for(int i = 0; i < length; i++) {
+    if (array[i] > array[i + 1]){
+        res=false;
+        swap(array, i, i + 1);
+    }
+}
+return res;
+}
+
+private static void swap(int[] array, int i, int j) {
+int tmp = array[i];
+array[i] = array[j];
+array[j] = tmp;
+}
+
+public static void sort(int [] array) {
+    int length = array.length;
+    boolean flSorted = false;
+    while (!flSorted){
+        length--;
+        flSorted = pushMaxAtEnd(array, length);
+}
+}
+/**
+ *
+ * @param array - sorted array
+ * @param key - being searched number
+ * @return see comments definition
+ */
+public static int binarySearch(int[] array, int key){
+    int low = 0;
+    int high = array.length - 1;
+    int mid = low + ((high - low) / 2);
+    while (low <= high) {
+        if (array[mid] == key) {
+            return mid;
+        }
+        else if (array[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+        mid = low + ((high - low) / 2);
+        if (low > high) {
+            break;
+        }
+    }
+    return -mid-1;
+}
+
+public static int[] insertSorted(int[] array, int number){
+    int keyIndex = Math.abs(binarySearch(array, number))-1;
+    array=insert(array, keyIndex, number);
+    return array;
+}
+public static boolean isOneSwap(int [] array, int [] sortedArray) {
+    int swap = 0;
+    for (int i = 0; i < array.length; i++) {
+        if (sortedArray[i] != array[i]){
+            swap++;
+        }
+    }
+if (swap == 2) {
+    return true;
+} else {
+    return false;
+}
 }
 }
--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -2,10 +2,16 @@ package telran.util.test;
 
 import org.junit.jupiter.api.Test;
 
+import telran.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static telran.util.Arrays.*;
 
+import java.util.Random;
+
 public class ArraysTest {
+private static final int N_ELEMENTS = 1_000;
+
 int[] numbers = {10, 7, 12, -4, 13, 3, 14};
 
 @Test
@@ -60,5 +66,71 @@ index = 4;
 int [] expected5 = {5,6,9,1};
 assertArrayEquals(expected5, remove(array, index));
 assertFalse(array.length==0);
+}
+
+ //HW6
+
+@Test
+void sortTest(){
+    int[]testArray = java.util.Arrays.copyOf(numbers, numbers.length);
+    int[] expected = {-4,3,7,10,12,13,14};
+    sort(testArray);
+    assertArrayEquals(expected, testArray);
+}
+
+@Test
+void sortTestRandomArray(){
+int[] array = getRandomArray(N_ELEMENTS);
+int limit = array.length-1;
+sort(array);
+for(int i = 0; i < limit; i++){
+assertTrue(array[i] <= array[i+1]);
+}
+}
+
+private int[] getRandomArray(int nElements) {
+    int[] res = new int[nElements];
+    Random random = new Random();
+    for(int i = 0; i< nElements; i++){
+        res[i] = random.nextInt(0,10000);
+    }
+    return res;
+}
+
+@Test
+void binarySearchTest(){
+assertFalse(numbers.length==0); //Check zero lenght of array
+int[] sortedArray = java.util.Arrays.copyOf(numbers, numbers.length); //Make a copy of origin Array for further sorting
+sort(sortedArray); //Sort array
+assertEquals(0, Arrays.binarySearch(sortedArray, -4)); // Checking binary search method for correct indexes
+assertEquals(3, Arrays.binarySearch(sortedArray, 10)); // Checking binary search method for correct indexes
+assertEquals(6, Arrays.binarySearch(sortedArray, 14)); // Checking binary search method for correct indexes
+assertEquals(-1, Arrays.binarySearch(sortedArray, -5)); //Checking binary search for out of array values. 0 should be at 0 position, so method must return -1 value (-index - 1)
+assertEquals(-8, Arrays.binarySearch(sortedArray, 15)); //Checking binary search for out of array values. 8 should be at 7 position, so method must return -8 value (-index - 1)
+}
+
+@Test
+void insertSortredTest(){
+assertFalse(numbers.length==0); //Check zero lenght of array
+int[] sortedArray = java.util.Arrays.copyOf(numbers, numbers.length); //Make a copy of origin Array for further sorting
+sort(sortedArray); //Sort array
+int[]expected = {-4,3,7,10,11,12,13,14};
+assertArrayEquals(expected, insertSorted(sortedArray, 11)); //Check if the "key" value is really at sorted array
+int[]expected2 = {-5,-4,3,7,10,12,13,14};
+assertArrayEquals(expected2, insertSorted(sortedArray, -5)); //Check if the "key" value is really at sorted array
+int[]expected3 = {-4,3,7,10,12,13,14,15};
+assertArrayEquals(expected3, insertSorted(sortedArray, 15)); //Check if the "key" value is really at sorted array
+}
+
+@Test
+void isOneSwapTest(){
+    int[]array = {1,2,3,9,5,6,7,8,4}; //Array with one swap to be sorted array
+    int[]sortedArray = java.util.Arrays.copyOf(array, array.length); //Make a copy of random Array for further sorting
+    sort(sortedArray);//Sort array
+    assertTrue(isOneSwap(array, sortedArray)); //Check if array need only one swap to be sorted
+    int[]array2 = {1,2,9,8,5,6,7,4,3};//Array with two swaps to be sorted array
+    int[]sortedArray2 = java.util.Arrays.copyOf(array, array.length); //Make a copy of random Array for further sorting
+    sort(sortedArray);//Sort array
+    assertFalse(isOneSwap(array2, sortedArray2)); //Check if array has more than one swap to be sorted
 }
 }
